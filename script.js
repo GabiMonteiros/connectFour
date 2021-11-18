6cb2e51202c6e2acb221283b58baf92e1f534e2d
Gabi Monteiro <contatogabimonteiro@gmail.com>
2021-11-18 17:26:43 +0100

    js checking empty slot

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 //console.log("halooo script, halloo Jq:", $)
 
 (function () {
-    var currentPlayer = $(".field-playerMoon");
+    var currentPlayer = "playerMoon";
 
+    //listening to the click in the column
+    $(".column").on("click", function (e) {
+        var columnElement = $(e.currentTarget);
+        var slotsInColumn = columnElement.children();
+
+        //loop to find empty slot from the bottom // i= slotsInColumn.length - 1 = 5
+        for (var i = 5; i >= 0; i--) {
+            if (!slot.hasClass("playerMoon") && !slot.hasClass("playerSun")) {
+                slot.addClass(currentPlayer);
+                break;
+                //break to stop the loop after find it
+            }
+        }
+    });
+
+    //track the current player
+    function switchPlayer() {
+        if (currentPlayer == "playerMoon") {
+            currentPlayer = "playerSun";
+        } else {
+            currentPlayer = "player";
+        }
+    }
 })();
