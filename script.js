64ef1a7cd4739df88cd009420b32255c429cf1ff
Gabi Monteiro <contatogabimonteiro@gmail.com>
2021-11-23 16:26:17 +0100

    fixing

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 (function game() {
     var currentPlayer = "playerMoon";
-    var board = $("#board")
+    var board = $("#board");
     var allslots = board.find(".slot");
-    var column =$(".column")
+    var column = $(".column");
     var winner = false;
     var round = 0;
     var playerMoon = 0;
@@ -16,15 +16,14 @@
     var playerSunField = $(".field-playerSun");
 
     //listening to the click in the column
+    //<!-- board = game , column = tr , slot = td  , hole = slot /// cell = hole
     $(".column").on("click", function (e) {
         var columnElement = $(e.currentTarget);
         var slotsInColumn = columnElement.children();
 
         //loop to find empty slot from the bottom // i= slotsInColumn.length - 1 = 5
         for (var i = 5; i >= 0; i--) {
-            if (!slot.hasClass("playerMoon") &&
-                !slot.hasClass("playerSun")
-            ) {
+            if (!slot.hasClass("playerMoon") && !slot.hasClass("playerSun")) {
                 slot.addClass(currentPlayer);
                 break;
                 //break to stop the loop after find it
@@ -44,14 +43,13 @@
         //checking for victory
         function checkForVictory(slots) {
             var count = 0;
-            for (var i = 0; i < slots.lenght; i++){
+            for (var i = 0; i < slots.lenght; i++) {
                 if (slots.eq(i).hasClass(currentPlayer)) {
                     count++;
                     if (count == 4) {
                         //victory
                         return true;
                     }
-
                 } else {
                     count = 0;
                 }
