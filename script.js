587e6115fde162712e161e64b6278fe3910e62a1
Gabi Monteiro <contatogabimonteiro@gmail.com>
2021-11-24 17:49:35 +0100

    js check victory, current player

--- script.js
+++ script.js
@@ -17,60 +17,86 @@
 
     //listening to the click in the column to run the function
     column.on("click", function (e) {
-        /*currentTarget sempre se refere ao elemento associado ao event handler, q nesse caso è o click na coluna. slot */
+        //currentTarget sempre se refere ao elemento associado ao event handler, q nesse caso è o click na coluna. slot */
         var slotsInColumn = $(e.currentTarget).children();
         //console.log('slotsInColumn: ', 'slotsInColumn');
 
         //loop to find empty slot from the bottom // i= slotsInColumn.length - 1 = 5
         for (var i = 5; i >= 0; i--) {
-            //eq(i) == equals  ao i 
-            //PAREI AQUI O QUE É HASCLASS
-              if (
+            //eq(i) == equals ao index, igual a  i ,  igual a um dos slots na coluna
+
+            if (
+                //se o islotsInColumn nao tem igual ao index preenchido pelo Moon e nao é igual ao Sun
                 !slotsInColumn.eq(i).hasClass("playerMoon") &&
                 !slotsInColumn.eq(i).hasClass("playerSun")
             ) {
                 slotsInColumn.eq(i).addClass(currentPlayer);
+                //add a class um deles(moon / sun)  como current player
                 break;
                 //break to stop the loop after find it
             }
         }
 
-        //checking empty slot
-        if (!foundEmpty) {
+        //checking empty slot on column
+        if (i == -1) {
             console.log("no empty slot, try again!");
-            return;
-        }
-        //so it ignores an element  if the hole is already filled
-        if (i < 0) {
             return;
         }
 
         //checking for victory
-        function checkForVictory(slots) {
-            var count = 0;
-            for (var i = 0; i < slots.lenght; i++) {
-                if (slots.eq(i).hasClass(currentPlayer)) {
-                    count++;
-                    if (count == 4) {
-                        //victory
-                        return true;
-                    }
-                } else {
-                    count = 0;
-                }
-            }
+        if (checkVictory(slotsInColumn)) {
+            //some player won in column
+            winner = true;
+            VictoryAnimation();
+            return;
+        } else {
+            var slotsInRow = $(".row" + i);
+            //para achar a row q o player ta , usa jquery a partir do loop backwards, com jq passo o index para row ??
+            if (checkVictory(slotsInRow)) {
+                //player won in column
+                winner = true;
+                VictoryAnimation();
+                return;
+            } //else {
+            //     checkDiagonalVictory();
+            // }
         }
 
-        //the game continues
+        //color for the currentPlayer
+        if (currentPlayer == "playerMoon") {
+            currentPlayerField.css({
+                backgroundcolor: "lightblue;",
+            });
+        } else {
+            currentPlayerField.css({
+                backgroundcolor: "orange;",
+            });
+        }
+        // //the game continues
         switchPlayers();
     });
 
-    //track the current player
+    function checkVictory(slots) {
+        var count = 0;
+        for (var i = 0; i < slots.length; i++) {
+            if (slots.eq(i).hasClass(currentPlayer)) {
+                count++;
+                if (count == 4) {
+                    // winner!!!
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+    }
+
+    // tracking urrent player
     function switchPlayers() {
         if (currentPlayer == "playerMoon") {
             currentPlayer = "playerSun";
         } else {
-            currentPlayer = "player";
+            currentPlayer = "playerMoon";
         }
     }
 })();
